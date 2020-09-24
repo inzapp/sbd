85d149f0d3cfc5ef1ac7b17f0e6685ab2a97dee4
inzapp <inzapp@naver.com>
2020-09-24 20:17:49 +0900

    refactor load function: segmentation output label -> white highlighted label change default shape spec as numpy shape remove epoch_end_multiple_output_callback function batch normalization after activation function

--- sbd.py
+++ sbd.py
@@ -32,14 +32,12 @@ lr = 0.01
 momentum = 0.9
 batch_size = 2
 epoch = 500
-grid = (32, 32)
-size = (256, 256)
+input_shape = (256, 256)
+output_shape = (32, 32)
 bbox_percentage_threshold = 0.5
-bbox_padding_val = 5
+bbox_padding_val = 3
 
 font_scale = 0.4
-grid_width = int(size[0] / grid[0])
-grid_height = int(size[1] / grid[1])
 img_channels = 3 if img_type == cv2.IMREAD_COLOR else 1
 class_count = 0
 class_names = []
@@ -48,36 +46,29 @@ class_names = []
 def load():
     global train_img_path, class_count, class_names
     with open(rf'{train_img_path}\classes.txt', 'rt') as classes_file:
-        class_names = classes_file.read().split('\n')
-        class_names.remove('')
+        class_names = [s.replace('\n', '') for s in classes_file.readlines()]
         class_count = len(class_names)
 
     img_paths = glob(rf'{train_img_path}\*.jpg') + glob(rf'{train_img_path}\*.png')
     total_x, total_y = [], [[] for _ in range(class_count)]
     for cur_img_path in tqdm(img_paths):
-        file_name_without_extension = cur_img_path.replace('\\', '/').split('/').pop().split('.')[0]
+        file_name_without_extension = cur_img_path.replace('\\', '/').split('/').pop()[:-4]
         x = cv2.imread(cur_img_path, img_type)
-        x = cv2.resize(x, size)
+        x = cv2.resize(x, (input_shape[1], input_shape[0]))
         total_x.append(x)
         with open(rf'{train_img_path}\{file_name_without_extension}.txt') as file:
-            masks = [np.zeros(size[0] * size[1]).reshape(size[0], size[1]) for _ in range(class_count)]
-            for line in file.readlines():
-                class_index, cx, cy, w, h = np.array(line.split(' ')).astype('float32')
-                x1, y1, x2, y2 = cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2
-                x1, y1, x2, y2 = int(x1 * size[0]), int(y1 * size[1]), int(x2 * size[0]), int(y2 * size[1])
-                cv2.rectangle(masks[int(class_index)], (x1, y1), (x2, y2), (255, 255, 255), -1)
-            for mask_index, _ in enumerate(masks):
-                segmentation_output = []
-                for col in range(0, size[0], grid_width):
-                    for row in range(0, size[1], grid_height):
-                        cur_grid = masks[mask_index][col:col + grid_height, row:row + grid_width]
-                        non_zero_count = cv2.countNonZero(cur_grid)
-                        cur_grid_segmentation_ratio = non_zero_count / (grid_width * grid_height)
-                        segmentation_output.append(cur_grid_segmentation_ratio)
-                total_y[mask_index].append(segmentation_output)
+            label_lines = file.readlines()
+        y = [np.zeros(output_shape, dtype=np.uint8) for _ in range(class_count)]
+        for label_line in label_lines:
+            class_index, cx, cy, w, h = list(map(float, label_line.split(' ')))
+            x1, y1, x2, y2 = cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2
+            x1, y1, x2, y2 = int(x1 * output_shape[1]), int(y1 * output_shape[0]), int(x2 * output_shape[1]), int(y2 * output_shape[0])
+            cv2.rectangle(y[int(class_index)], (x1, y1), (x2, y2), (255, 255, 255), -1)
+        for i in range(class_count):
+            total_y[i].append(y[i])
 
-    total_x = np.asarray(total_x).reshape(len(total_x), size[0], size[1], img_channels).astype('float32') / 255
-    total_y = np.asarray(total_y).reshape(class_count, len(total_y[0]), grid[0], grid[1]).astype('float32')
+    total_x = np.asarray(total_x).reshape(len(total_x), input_shape[0], input_shape[1], img_channels).astype('float32') / 255.
+    total_y = np.asarray(total_y).reshape(class_count, len(total_y[0]), output_shape[0], output_shape[1]).astype('float32') / 255.
     r = np.arange(len(total_x))
     np.random.shuffle(r)
     total_x = total_x[r]
@@ -89,39 +80,17 @@ def load():
     return total_x, new_total_y
 
 
-def epoch_end_multiple_output_callback(cur_epoch, logs):
-    global class_count
-    min_loss, max_loss, sum_of_avg_loss = 999999999, 0, 0
-    print(f'epoch {cur_epoch}')
-    for i in range(class_count):
-        cur_loss = logs[f'r{i}_loss']
-        min_loss = cur_loss if cur_loss < min_loss else min_loss
-        max_loss = cur_loss if cur_loss > max_loss else max_loss
-        sum_of_avg_loss += cur_loss
-    avg_loss = sum_of_avg_loss / class_count
-    print(f'min_loss : {min_loss:.4f}, max_loss : {max_loss:.4f}, avg_loss : {avg_loss:.4f}')
-    min_loss, max_loss, sum_of_avg_loss = 999999999, 0, 0
-    for i in range(class_count):
-        cur_loss = logs[f'val_r{i}_loss']
-        min_loss = cur_loss if cur_loss < min_loss else min_loss
-        max_loss = cur_loss if cur_loss > max_loss else max_loss
-        sum_of_avg_loss += cur_loss
-    avg_loss = sum_of_avg_loss / class_count
-    print(f'min_val_loss : {min_loss:.4f}, max_val_loss : {max_loss:.4f}, avg_val_loss : {avg_loss:.4f}')
-    print()
-
-
 def predict(model, img):
     global bbox_percentage_threshold, bbox_padding_val
-    x = cv2.resize(img, size)
-    x = np.array(x).reshape(1, size[0], size[1], img_channels).astype('float32') / 255
+    x = cv2.resize(img, (input_shape[1], input_shape[0]))
+    x = np.array(x).reshape(1, input_shape[0], input_shape[1], img_channels).astype('float32') / 255
     res_list = model.predict(x=x, batch_size=1)
     predict_res = []
     for i, res in enumerate(res_list):
-        res = np.array(res).reshape(grid[0], grid[1], 1).astype('float32') * 255
+        res = np.array(res).reshape(output_shape[0], output_shape[1], 1).astype('float32') * 255
         res = cv2.resize(res, (img.shape[1], img.shape[0])).astype('uint8')
         _, res = cv2.threshold(res, int(bbox_percentage_threshold * 255), 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(res, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(res, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
             x, y, w, h = x - bbox_padding_val, y - bbox_padding_val, w + 2 * bbox_padding_val, h + 2 * bbox_padding_val
@@ -134,7 +103,7 @@ def predict(model, img):
 
 def get_text_label_width_height(text):
     global font_scale
-    black = np.zeros(50 * 300).reshape(50, 300).astype('uint8')
+    black = np.zeros((50, 500), dtype=np.uint8)
     cv2.putText(black, text, (30, 30), cv2.FONT_HERSHEY_DUPLEX, fontScale=font_scale, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
     black = cv2.resize(black, (0, 0), fx=0.5, fy=0.5)
     black = cv2.dilate(black, np.ones((2, 3), dtype=np.uint8), iterations=2)
@@ -173,41 +142,37 @@ def train():
     global alpha, lr, momentum, batch_size, epoch, test_img_path, class_names
     total_x, total_y = load()
 
-    model_input = Input(shape=(size[0], size[1], img_channels))
-
-    x = layers.Conv2D(filters=4, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(model_input)
-    x = layers.BatchNormalization()(x)
+    model_input = Input(shape=(input_shape[0], input_shape[1], img_channels))
+    x = layers.Conv2D(filters=16, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(model_input)
     x = layers.LeakyReLU(alpha=alpha)(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPool2D()(x)
 
-    x = layers.Conv2D(filters=8, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=32, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(x)
     x = layers.LeakyReLU(alpha=alpha)(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPool2D()(x)
 
-    x = layers.Conv2D(filters=16, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=64, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(x)
     x = layers.LeakyReLU(alpha=alpha)(x)
-    x = layers.Conv2D(filters=16, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=128, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(x)
     x = layers.LeakyReLU(alpha=alpha)(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPool2D()(x)
 
-    x = layers.Conv2D(filters=32, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=256, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(x)
     x = layers.LeakyReLU(alpha=alpha)(x)
-    x = layers.Conv2D(filters=32, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=512, kernel_size=(3, 3), kernel_initializer='he_normal', padding='same')(x)
     x = layers.LeakyReLU(alpha=alpha)(x)
-
     model_outputs = []
     for i in range(class_count):
         model_output = layers.Conv2D(filters=1, kernel_size=(1, 1), padding='same', activation='sigmoid')(x)
-        model_output = layers.Reshape(target_shape=(grid[0], grid[1]), name=f'{class_names[i]}')(model_output)
+        model_output = layers.Reshape(target_shape=output_shape, name=f'{class_names[i]}')(model_output)
         model_outputs.append(model_output)
 
     model = Model(model_input, model_outputs)
-
     model.summary()
     model.compile(optimizer=SGD(lr=lr, momentum=momentum), loss='binary_crossentropy')
     model.fit(
