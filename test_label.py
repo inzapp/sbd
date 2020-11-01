0d8f42e40e8060a92ccf8bdb68291f228807aa64
inzapp <inzapp@naver.com>
2020-11-01 16:08:21 +0900

    refactor test_label

--- test_label.py
+++ test_label.py
@@ -4,27 +4,23 @@ import cv2
 
 import sbd
 
-import numpy as np
-
 
 def test_label():
     """
-    test sbd label(yolo format) is validate
+    test if sbd label(yolo format) is validate
     """
     with open(rf'{sbd.train_img_path}\classes.txt', 'rt') as classes_file:
         sbd.class_names = classes_file.readlines()
     img_paths = glob(rf'{sbd.train_img_path}\*.jpg') + glob(rf'{sbd.train_img_path}\*.png')
     for cur_img_path in img_paths:
         img = cv2.imread(cur_img_path, sbd.img_type)
-        img_height, img_width, c = img.shape
-        file_name_without_extension = cur_img_path.replace('\\', '/').split('/').pop().split('.')[0]
         converted_res = []
-        with open(rf'{sbd.train_img_path}/{file_name_without_extension}.txt', 'rt') as f:
-            lines = f.read().strip().split('\n')
+        with open(rf'{cur_img_path[:-4]}.txt', mode='rt') as f:
+            lines = f.readlines()
         for line in lines:
-            class_index, cx, cy, w, h = np.array(line.split(' ')).astype('float32')
+            class_index, cx, cy, w, h = list(map(float, line.split(' ')))
             x1, y1, x2, y2 = cx - w / 2, cy - h / 2, cx + w / 2, cy + h / 2
-            x1, y1, x2, y2 = int(x1 * img_width), int(y1 * img_height), int(x2 * img_width), int(y2 * img_height)
+            x1, y1, x2, y2 = int(x1 * img.shape[1]), int(y1 * img.shape[0]), int(x2 * img.shape[1]), int(y2 * img.shape[0])
             converted_res.append({'class': class_index, 'box': [x1, y1, x2, y2]})
         img = sbd.bounding_box(img, converted_res)
         cv2.imshow('img', img)
