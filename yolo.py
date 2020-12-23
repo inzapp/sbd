9f5524e3658cee1451853e7178e0a412634fbca8
inzapp <inzapp@naver.com>
2020-12-23 09:02:04 +0900

    c

--- yolo.py
+++ yolo.py
@@ -30,7 +30,7 @@ img_type = cv2.IMREAD_GRAYSCALE
 train_image_path = r'C:\inz\train_data\lp_detection_yolo'
 test_image_path = r'C:\inz\train_data\lp_detection_yolo'
 
-lr = 1e-4
+lr = 1e-5
 batch_size = 2
 epoch = 10000
 validation_ratio = 0.2
@@ -334,6 +334,7 @@ def train():
 
     x = tf.keras.layers.Conv2D(filters=class_count + 5, kernel_size=1, activation='sigmoid')(x)
     model = tf.keras.models.Model(model_input, x)
+    model = tf.keras.models.load_model('checkpoints/0.1M_epoch_8_loss_0.000337_val_loss_0.000513.h5', compile=False)
 
     model.summary()
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr), loss=MeanAbsoluteLogError())
@@ -355,7 +356,7 @@ def train():
         validation_data=validation_data_generator,
         epochs=epoch,
         callbacks=[
-            tf.keras.callbacks.ModelCheckpoint(filepath='checkpoints/0.1M_epoch_{epoch}_loss_{loss:.6f}_val_loss_{val_loss:.6f}.h5'),
+            tf.keras.callbacks.ModelCheckpoint(filepath='checkpoints/2_0.1M_epoch_{epoch}_loss_{loss:.6f}_val_loss_{val_loss:.6f}.h5'),
             tf.keras.callbacks.ModelCheckpoint(filepath='model.h5'),
             tf.keras.callbacks.LambdaCallback(on_batch_end=random_live_view),
         ]
@@ -467,7 +468,7 @@ def count_test():
 
 if __name__ == '__main__':
     # count_test()
-    train()
-    # freeze('checkpoints/yolo_epoch_134_loss_0.0000_val_loss_0.0004.h5')
+    # train()
+    freeze('checkpoints/2_0.1M_epoch_300_loss_0.000038_val_loss_0.000571.h5')
     # freeze('model.h5')
-    # test_video()
+    test_video()
