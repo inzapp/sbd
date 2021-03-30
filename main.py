94cbc1a6b2d280892e80222eee0e568566741d29
inzapp <inzapp@naver.com>
2021-03-30 10:33:58 +0900

    c

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ if __name__ == '__main__':
 
     model = Yolo()
     model.fit(
-        train_image_path=r'\\192.168.101.200\train_data\person_data_train',
-        validation_image_path=r'\\192.168.101.200\train_data\person_data_validation',
+        train_image_path=r'/home2/train_data/person_data_train/',
+        validation_image_path=r'/home2/train_data/person_data_validation/',
         model_name='person_info_detector',
         input_shape=(256, 128, 3),
         batch_size=2,
@@ -71,58 +71,3 @@ if __name__ == '__main__':
         mixed_float16_training=False,
         use_map_callback=True)
 
-    # model = Yolo()
-    # model.fit(
-    #     train_image_path=r'C:\inz\train_data\lp_character_detection\lcd_b1',
-    #     model_name='v1_lcd_b1_288_144',
-    #     input_shape=(144, 288, 1),
-    #     batch_size=2,
-    #     lr=1e-3,
-    #     epochs=300,
-    #     curriculum_epochs=0,
-    #     validation_split=0.2,
-    #     training_view=False,
-    #     mixed_float16_training=False)
-    #
-    # model = Yolo()
-    # model.fit(
-    #     train_image_path=r'C:\inz\train_data\lp_character_detection\lcd_b2',
-    #     model_name='v1_lcd_b2_288_144',
-    #     input_shape=(144, 288, 1),
-    #     batch_size=2,
-    #     lr=1e-3,
-    #     epochs=300,
-    #     curriculum_epochs=0,
-    #     validation_split=0.2,
-    #     training_view=False,
-    #     mixed_float16_training=False)
-    #
-    # model = Yolo()
-    # model.fit(
-    #     train_image_path=r'C:\inz\train_data\lp_character_detection\lcd_biz',
-    #     model_name='v1_lcd_biz_288_144',
-    #     input_shape=(144, 288, 1),
-    #     batch_size=2,
-    #     lr=1e-3,
-    #     epochs=300,
-    #     curriculum_epochs=0,
-    #     validation_split=0.2,
-    #     training_view=False,
-    #     mixed_float16_training=False)
-    #
-    # model = Yolo()
-    # model.fit(
-    #     train_image_path=r'C:\inz\train_data\lp_character_detection\lcd_white',
-    #     model_name='v1_lcd_white_288_144',
-    #     input_shape=(144, 288, 1),
-    #     batch_size=2,
-    #     lr=1e-3,
-    #     epochs=300,
-    #     curriculum_epochs=0,
-    #     validation_split=0.2,
-    #     training_view=False,
-    #     mixed_float16_training=False)
-
-    # model = Yolo(pretrained_model_path=r'person_detector_model_epoch_17_f1_0.5952_val_f1_0.1739.h5', class_names_file_path=r'X:\person_data\classes.txt')
-    # from glob import glob
-    # model.predict_images(glob(r'X:\person_data\*.jpg'))
