11b74be8d36271f67ed1a52e15050b8a65c03483
inzapp <inzapp@naver.com>
2021-04-13 13:13:10 +0900

    update person info detector model

--- model.py
+++ model.py
@@ -37,9 +37,9 @@ class Model:
         # return self.__vgg_19()
         # return self.__darknet_53()
         # return self.__build_loon_detector()
-        # return self.__build_person_info_detector()
+        return self.__build_person_info_detector()
         # return self.__build_lcd()
-        return self.__build_lcd_cv2()
+        # return self.__build_lcd_cv2()
         # return self.__build_sbd()
 
     # just for testing
@@ -53,15 +53,16 @@ class Model:
         x = self.__point_wise_conv(self.__output_channel, x)
         return tf.keras.models.Model(input_layer, x)
 
+    # input_shape=(128, 64, 1) or input_shape=(192, 96, 1)
     def __build_person_info_detector(self):
         input_layer = tf.keras.layers.Input(shape=self.__input_shape)
         x = self.__conv_block(32, 3, input_layer, True)
-        x = self.__conv_block(64, 3, x)
         x = self.__conv_block(64, 3, x, True)
-        x = self.__conv_block(128, 3, x)
+        x = self.__conv_block(64, 3, x, True)
+        x = self.__conv_block(128, 3, x, True)
         x = self.__conv_block(128, 3, x, True)
         x = self.__conv_block(256, 3, x)
-        x = self.__conv_block(256, 3, x, True)
+        x = self.__conv_block(256, 3, x)
         x = self.__conv_block(512, 3, x)
         x = self.__point_wise_conv(self.__output_channel, x)
         return tf.keras.models.Model(input_layer, x)
