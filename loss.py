cc8b8da3616d939d85373c63d67de9fa029a298c
inzapp <inzapp@naver.com>
2021-07-28 14:16:21 +0900

    [iou loss] iou confidence loss success

--- loss.py
+++ loss.py
@@ -17,6 +17,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework.ops import convert_to_tensor_v2
 
@@ -49,66 +50,63 @@ class ConfidenceLoss(tf.keras.losses.Loss):
         obj_pred = y_pred[:, :, :, 0]
         obj_false = (tf.ones(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)) - obj_true
 
-        obj_confidence_loss = tf.reduce_sum(tf.square(smooth(obj_true, true_only=True) - (obj_pred * obj_true)))
-        # obj_confidence_loss = tf.reduce_sum(tf.square(obj_true - self.iou(y_true, y_pred, obj_true)))  # TODO : iou loss
+        # obj_confidence_loss = tf.reduce_sum(tf.square(smooth(obj_true, true_only=True) - (obj_pred * obj_true)))
+        obj_confidence_loss = tf.reduce_sum(tf.square((obj_pred * obj_true) - (self.iou(y_true, y_pred) * obj_true)))  # TODO : iou loss
 
-        no_obj_confidence_loss = tf.reduce_sum(tf.square((obj_pred * obj_false) - tf.zeros(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)))
+        no_obj_confidence_loss = tf.reduce_sum(tf.square((obj_pred * obj_false) - tf.zeros(shape=tf.shape(obj_false), dtype=tf.dtypes.float32)))
         return obj_confidence_loss + (no_obj_confidence_loss * self.no_obj)
 
     @staticmethod
-    def iou(y_true, y_pred, obj_true):
+    def iou(y_true, y_pred):
         y_true_shape = tf.cast(tf.shape(y_true), dtype=tf.dtypes.float32)
-        num_samples, grid_height, grid_width = y_true_shape[0], y_true_shape[1], y_true_shape[2]
+        grid_height, grid_width = y_true_shape[1], y_true_shape[2]
+
+        cx_true = y_true[:, :, :, 1]
+        cy_true = y_true[:, :, :, 2]
+        w_true = y_true[:, :, :, 3]
+        h_true = y_true[:, :, :, 4]
 
         x_range = tf.range(grid_width, dtype=tf.dtypes.float32)
-        y_range = tf.transpose(tf.reshape(tf.range(grid_height, dtype=tf.dtypes.float32), shape=(1, grid_height, 1)))
+        x_offset = tf.broadcast_to(x_range, shape=tf.shape(cx_true))
 
-        x_offset = (x_range + tf.zeros(shape=(num_samples, grid_height, grid_width))) / grid_width * obj_true
-        y_offset = (y_range + tf.zeros(shape=(num_samples, grid_height, grid_width))) / grid_height * obj_true
+        y_range = tf.range(grid_height, dtype=tf.dtypes.float32)
+        y_range = tf.reshape(y_range, shape=(1, grid_height, 1))
+        y_offset = tf.broadcast_to(y_range, shape=tf.shape(cy_true))
 
-        y_true_cx = x_offset + (y_true[:, :, :, 1] * 1.0 / grid_width)
-        y_true_cy = y_offset + (y_true[:, :, :, 2] * 1.0 / grid_height)
-        y_true_w = y_true[:, :, :, 3]
-        y_true_h = y_true[:, :, :, 4]
+        cx_true = x_offset + (cx_true * 1.0 / grid_width)
+        cy_true = y_offset + (cy_true * 1.0 / grid_height)
 
-        y_true_x1 = y_true_cx - y_true_w / 2.0
-        y_true_y1 = y_true_cy - y_true_h / 2.0
-        y_true_x2 = y_true_cx + y_true_w / 2.0
-        y_true_y2 = y_true_cy + y_true_h / 2.0
+        x1_true = cx_true - w_true / 2.0
+        y1_true = cy_true - h_true / 2.0
+        x2_true = cx_true + w_true / 2.0
+        y2_true = cy_true + h_true / 2.0
 
-        y_true_x1 *= 1000.0
-        y_true_y1 *= 1000.0
-        y_true_x2 *= 1000.0
-        y_true_y2 *= 1000.0
+        cx_pred = y_pred[:, :, :, 1]
+        cy_pred = y_pred[:, :, :, 2]
+        w_pred = y_pred[:, :, :, 3]
+        h_pred = y_pred[:, :, :, 4]
 
-        y_pred_cx = (x_offset + (y_pred[:, :, :, 1] * obj_true * 1.0 / grid_width)) * obj_true
-        y_pred_cy = (y_offset + (y_pred[:, :, :, 2] * obj_true * 1.0 / grid_height)) * obj_true
-        y_pred_w = y_pred[:, :, :, 3] * obj_true
-        y_pred_h = y_pred[:, :, :, 4] * obj_true
+        cx_pred = x_offset + (cx_pred * 1.0 / grid_width)
+        cy_pred = y_offset + (cy_pred * 1.0 / grid_height)
 
-        y_pred_x1 = y_pred_cx - y_pred_w / 2.0
-        y_pred_y1 = y_pred_cy - y_pred_h / 2.0
-        y_pred_x2 = y_pred_cx + y_pred_w / 2.0
-        y_pred_y2 = y_pred_cy + y_pred_h / 2.0
+        x1_pred = cx_pred - w_pred / 2.0
+        y1_pred = cy_pred - h_pred / 2.0
+        x2_pred = cx_pred + w_pred / 2.0
+        y2_pred = cy_pred + h_pred / 2.0
 
-        y_pred_x1 *= 1000.0
-        y_pred_y1 *= 1000.0
-        y_pred_x2 *= 1000.0
-        y_pred_y2 *= 1000.0
-
-        min_x2 = tf.minimum(y_true_x2, y_pred_x2)
-        max_x1 = tf.maximum(y_true_x1, y_pred_x1)
-        min_y2 = tf.minimum(y_true_y2, y_pred_y2)
-        max_y1 = tf.maximum(y_true_y1, y_pred_y1)
+        min_x2 = tf.minimum(x2_true, x2_pred)
+        max_x1 = tf.maximum(x1_true, x1_pred)
+        min_y2 = tf.minimum(y2_true, y2_pred)
+        max_y1 = tf.maximum(y1_true, y1_pred)
 
         intersection_width = tf.maximum(min_x2 - max_x1, 0.0)
         intersection_height = tf.maximum(min_y2 - max_y1, 0.0)
         intersection = intersection_width * intersection_height
 
-        y_true_area = y_true_w * y_true_h
-        y_pred_area = y_pred_w * y_pred_h
+        y_true_area = w_true * h_true
+        y_pred_area = w_pred * h_pred
         union = y_true_area + y_pred_area - intersection
-        return intersection / (union + 1e-4)
+        return tf.truediv(intersection, union)
 
 
 class ConfidenceWithBoundingBoxLoss(tf.keras.losses.Loss):
