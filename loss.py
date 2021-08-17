a36d34c1587b87c532f4368eb265f1a14788d87c
inzapp <inzapp@naver.com>
2021-08-17 11:35:24 +0900

    [loss.py] function refactor

--- loss.py
+++ loss.py
@@ -21,96 +21,78 @@ import tensorflow as tf
 from tensorflow.python.framework.ops import convert_to_tensor_v2
 
 
-def smooth(y_true, alpha=0.1, true_only=False):
-    smooth_true = tf.clip_by_value(y_true - alpha, 0.0, 1.0)
-    if true_only:
-        return smooth_true
-    smooth_false = tf.clip_by_value(((y_true + alpha) * -1.0 + alpha * 2.0), 0.0, 1.0)
-    return smooth_true + smooth_false
+def __confidence_loss(y_true, y_pred):
+    no_obj = 0.5
+    obj_true = y_true[:, :, :, 0]
+    obj_pred = y_pred[:, :, :, 0]
+    obj_false = tf.ones(shape=tf.shape(obj_true), dtype=tf.dtypes.float32) - obj_true
+
+    obj_confidence_loss = tf.keras.backend.binary_crossentropy(obj_true, obj_pred, from_logits=False) * obj_true
+    obj_confidence_loss = tf.reduce_mean(obj_confidence_loss, axis=0)
+    obj_confidence_loss = tf.reduce_sum(obj_confidence_loss)
+
+    zeros = tf.zeros(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)
+    no_obj_confidence_loss = tf.keras.backend.binary_crossentropy(obj_pred, zeros, from_logits=False) * obj_false
+    no_obj_confidence_loss = tf.reduce_mean(no_obj_confidence_loss, axis=0)
+    no_obj_confidence_loss = tf.reduce_sum(no_obj_confidence_loss)
+    return obj_confidence_loss + (no_obj_confidence_loss * no_obj)
 
 
-class ConfidenceLoss(tf.keras.losses.Loss):
+def __bbox_loss(y_true, y_pred):
     """
-    This loss function is used to reduce the loss of the confidence channel with some epochs before training begins.
+    BCE (sqrt(obj(x))) at width and height regression loss
+    Sqrt was used to weight the width, height loss for small boxes.
+
+    To avoid dividing by zero when going through the derivative formula of sqrt,
+    Adds the eps value to the sqrt parameter.
+
+    Derivative of sqrt : 1 / (2 * sqrt(x))
     """
+    obj_true = y_true[:, :, :, 0]
 
-    def __init__(self, no_obj=0.5):
-        """
-        :param no_obj: value for reduce gradient of confidence channel where no object. 0.5 is recommendation value in yolo paper.
-        """
-        self.no_obj = no_obj
-        super(ConfidenceLoss, self).__init__()
+    xy_true = y_true[:, :, :, 1:3]
+    xy_pred = y_pred[:, :, :, 1:3]
+    xy_loss = tf.keras.backend.binary_crossentropy(xy_true, xy_pred, from_logits=False)
+    xy_loss = tf.reduce_sum(xy_loss, axis=-1) * obj_true
+    xy_loss = tf.reduce_mean(xy_loss, axis=0)
+    xy_loss = tf.reduce_sum(xy_loss)
 
-    def call(self, y_true, y_pred):
-        y_pred = convert_to_tensor_v2(y_pred)
-        y_true = tf.cast(y_true, y_pred.dtype)
-
-        obj_true = y_true[:, :, :, 0]
-        obj_pred = y_pred[:, :, :, 0]
-        obj_false = (tf.ones(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)) - obj_true
-        obj_confidence_loss = tf.reduce_sum(tf.square(obj_true - (obj_pred * obj_true)))
-        no_obj_confidence_loss = tf.reduce_sum(tf.square((obj_pred * obj_false) - tf.zeros(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)))
-        return obj_confidence_loss + (no_obj_confidence_loss * self.no_obj)
-
-
-class ConfidenceWithBoundingBoxLoss(tf.keras.losses.Loss):
-    """
-    This loss function is used to reduce the loss of the confidence and bounding box channel with some epochs before training begins.
-    """
-
-    def __init__(self, coord=5.0):
-        """
-        :param coord: coord value of bounding box loss. 5.0 is recommendation value in yolo paper.
-        """
-        self.coord = coord
-        super(ConfidenceWithBoundingBoxLoss, self).__init__()
-
-    def call(self, y_true, y_pred):
-        y_pred = convert_to_tensor_v2(y_pred)
-        y_true = tf.cast(y_true, y_pred.dtype)
-        confidence_loss = ConfidenceLoss()(y_true, y_pred)
-        """
-        SSE at x, y regression
-        """
-        confidence_true = y_true[:, :, :, 0]
-
-        x_loss = tf.reduce_sum(tf.square(y_true[:, :, :, 1] - (y_pred[:, :, :, 1] * confidence_true)))
-        y_loss = tf.reduce_sum(tf.square(y_true[:, :, :, 2] - (y_pred[:, :, :, 2] * confidence_true)))
-
-        """
-        SSE (sqrt(obj(x))) at width and height regression loss
-        Sqrt was used to weight the width, height loss for small boxes.
-        
-        To avoid dividing by zero when going through the derivative formula of sqrt,
-        Adds the eps value to the sqrt parameter.
-        
-        Derivative of sqrt : 1 / (2 * sqrt(x))
-        """
-        w_true = tf.sqrt(y_true[:, :, :, 3] + 1e-4)
-        w_pred = tf.sqrt(y_pred[:, :, :, 3] + 1e-4)
-        w_loss = tf.reduce_sum(tf.square(w_true - (w_pred * confidence_true)))
-        h_true = tf.sqrt(y_true[:, :, :, 4] + 1e-4)
-        h_pred = tf.sqrt(y_pred[:, :, :, 4] + 1e-4)
-        h_loss = tf.reduce_sum(tf.square(h_true - (h_pred * confidence_true)))
-        bbox_loss = x_loss + y_loss + w_loss + h_loss
-        return confidence_loss + (bbox_loss * self.coord)
+    eps = tf.keras.backend.epsilon()
+    wh_true = tf.sqrt(y_true[:, :, :, 3:5] + eps)
+    wh_pred = tf.sqrt(y_pred[:, :, :, 3:5] + eps)
+    wh_loss = tf.keras.backend.binary_crossentropy(wh_true, wh_pred, from_logits=False)
+    wh_loss = tf.reduce_sum(wh_loss, axis=-1) * obj_true
+    wh_loss = tf.reduce_mean(wh_loss, axis=0)
+    wh_loss = tf.reduce_sum(wh_loss)
+    bbox_loss = xy_loss + wh_loss
+    return bbox_loss
 
 
-class YoloLoss(tf.keras.losses.Loss):
-    def __init__(self):
-        super(YoloLoss, self).__init__()
+def __classification_loss(y_true, y_pred):
+    obj_true = y_true[:, :, :, 0]
+    classification_loss = tf.keras.backend.binary_crossentropy(y_true[:, :, :, 5:], y_pred[:, :, :, 5:], from_logits=False)
+    classification_loss = tf.reduce_sum(classification_loss, axis=-1) * obj_true
+    classification_loss = tf.reduce_mean(classification_loss, axis=0)
+    classification_loss = tf.reduce_sum(classification_loss)
+    return classification_loss
 
-    def call(self, y_true, y_pred):
-        y_pred = convert_to_tensor_v2(y_pred)
-        y_true = tf.cast(y_true, y_pred.dtype)
 
-        y_pred = tf.sigmoid(y_pred)
+def confidence_loss(y_true, y_pred):
+    y_pred = convert_to_tensor_v2(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    y_pred = tf.sigmoid(y_pred)
+    return __confidence_loss(y_true, y_pred)
 
-        confidence_bbox_loss = ConfidenceWithBoundingBoxLoss()(y_true, y_pred)
 
-        """
-        SSE at all classification
-        """
-        confidence_true = y_true[:, :, :, 0]
-        classification_loss = tf.reduce_sum(tf.reduce_sum(tf.square(smooth(y_true[:, :, :, 5:]) - y_pred[:, :, :, 5:]), axis=-1) * confidence_true)
-        return confidence_bbox_loss + classification_loss
+def confidence_with_bbox_loss(y_true, y_pred):
+    y_pred = convert_to_tensor_v2(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    y_pred = tf.sigmoid(y_pred)
+    return __confidence_loss(y_true, y_pred) + __bbox_loss(y_true, y_pred)
+
+
+def yolo_loss(y_true, y_pred):
+    y_pred = convert_to_tensor_v2(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    y_pred = tf.sigmoid(y_pred)
+    return __confidence_loss(y_true, y_pred) + __bbox_loss(y_true, y_pred) + __classification_loss(y_true, y_pred)
