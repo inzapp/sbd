5633f660b17606de5356311373736a0e94c67a91
inzapp <inzapp@naver.com>
2021-08-17 19:57:56 +0900

    add iou function in loss.py

--- loss.py
+++ loss.py
@@ -28,14 +28,67 @@ def __confidence_loss(y_true, y_pred):
     obj_false = tf.ones(shape=tf.shape(obj_true), dtype=tf.dtypes.float32) - obj_true
 
     obj_confidence_loss = tf.keras.backend.binary_crossentropy(obj_true, obj_pred, from_logits=False) * obj_true
+    # obj_confidence_loss = tf.keras.backend.binary_crossentropy(__iou(y_true, y_pred), obj_pred, from_logits=False) * obj_true
     obj_confidence_loss = tf.reduce_mean(obj_confidence_loss, axis=0)
     obj_confidence_loss = tf.reduce_sum(obj_confidence_loss)
 
     zeros = tf.zeros(shape=tf.shape(obj_true), dtype=tf.dtypes.float32)
-    no_obj_confidence_loss = tf.keras.backend.binary_crossentropy(obj_pred, zeros, from_logits=False) * obj_false
+    no_obj_confidence_loss = tf.keras.backend.binary_crossentropy(zeros, obj_pred, from_logits=False) * obj_false
     no_obj_confidence_loss = tf.reduce_mean(no_obj_confidence_loss, axis=0)
     no_obj_confidence_loss = tf.reduce_sum(no_obj_confidence_loss)
     return obj_confidence_loss + (no_obj_confidence_loss * no_obj)
+
+
+def __iou(y_true, y_pred):
+    y_true_shape = tf.cast(tf.shape(y_true), dtype=tf.dtypes.float32)
+    grid_height, grid_width = y_true_shape[1], y_true_shape[2]
+
+    cx_true = y_true[:, :, :, 1]
+    cy_true = y_true[:, :, :, 2]
+    w_true = y_true[:, :, :, 3]
+    h_true = y_true[:, :, :, 4]
+
+    x_range = tf.range(grid_width, dtype=tf.dtypes.float32)
+    x_offset = tf.broadcast_to(x_range, shape=tf.shape(cx_true))
+
+    y_range = tf.range(grid_height, dtype=tf.dtypes.float32)
+    y_range = tf.reshape(y_range, shape=(1, grid_height, 1))
+    y_offset = tf.broadcast_to(y_range, shape=tf.shape(cy_true))
+
+    cx_true = x_offset + (cx_true * 1.0 / grid_width)
+    cy_true = y_offset + (cy_true * 1.0 / grid_height)
+
+    x1_true = cx_true - w_true / 2.0
+    y1_true = cy_true - h_true / 2.0
+    x2_true = cx_true + w_true / 2.0
+    y2_true = cy_true + h_true / 2.0
+
+    cx_pred = y_pred[:, :, :, 1]
+    cy_pred = y_pred[:, :, :, 2]
+    w_pred = y_pred[:, :, :, 3]
+    h_pred = y_pred[:, :, :, 4]
+
+    cx_pred = x_offset + (cx_pred * 1.0 / grid_width)
+    cy_pred = y_offset + (cy_pred * 1.0 / grid_height)
+
+    x1_pred = cx_pred - w_pred / 2.0
+    y1_pred = cy_pred - h_pred / 2.0
+    x2_pred = cx_pred + w_pred / 2.0
+    y2_pred = cy_pred + h_pred / 2.0
+
+    min_x2 = tf.minimum(x2_true, x2_pred)
+    max_x1 = tf.maximum(x1_true, x1_pred)
+    min_y2 = tf.minimum(y2_true, y2_pred)
+    max_y1 = tf.maximum(y1_true, y1_pred)
+
+    intersection_width = tf.maximum(min_x2 - max_x1, 0.0)
+    intersection_height = tf.maximum(min_y2 - max_y1, 0.0)
+    intersection = intersection_width * intersection_height
+
+    y_true_area = w_true * h_true
+    y_pred_area = w_pred * h_pred
+    union = y_true_area + y_pred_area - intersection
+    return intersection / (union + 1e-4)
 
 
 def __bbox_loss(y_true, y_pred):
