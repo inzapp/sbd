550dfd4e109fb406948682c462e8ea2a49ee2e50
inzapp <inzapp@naver.com>
2021-08-29 15:13:12 +0900

    [generator best iou refactor] map output layer index with best iou with clustered box

--- generator.py
+++ generator.py
@@ -25,6 +25,7 @@ from time import sleep
 import cv2
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 
 
 class YoloDataGenerator:
@@ -68,14 +69,15 @@ class GeneratorFlow(tf.keras.utils.Sequence):
         self.input_shape = input_shape
         self.output_shapes = output_shapes
         self.batch_size = batch_size
-        self.random_indexes = np.arange(len(self.image_paths))
+        self.num_output_layers = len(output_shapes)
+        self.clustered_ws = []
+        self.clustered_hs = []
         self.pool = ThreadPoolExecutor(8)
 
         queue_size = 64
         self.batch_index = 0
         self.batch_q = Queue(maxsize=queue_size)
         self.insert_thread_running = False
-        np.random.shuffle(self.random_indexes)
 
     def __len__(self):
         """
@@ -99,6 +101,97 @@ class GeneratorFlow(tf.keras.utils.Sequence):
             insert_thread = Thread(target=self.__insert_batch_into_q)
             insert_thread.setDaemon(True)
             insert_thread.start()
+
+    @staticmethod
+    def __iou(a, b):
+        """
+        Intersection of union function.
+        :param a: [x_min, y_min, x_max, y_max] format box a
+        :param b: [x_min, y_min, x_max, y_max] format box b
+        """
+        a_x_min, a_y_min, a_x_max, a_y_max = a
+        b_x_min, b_y_min, b_x_max, b_y_max = b
+        intersection_width = min(a_x_max, b_x_max) - max(a_x_min, b_x_min)
+        intersection_height = min(a_y_max, b_y_max) - max(a_y_min, b_y_min)
+        if intersection_width <= 0 or intersection_height <= 0:
+            return 0.0
+        intersection_area = intersection_width * intersection_height
+        a_area = abs((a_x_max - a_x_min) * (a_y_max - a_y_min))
+        b_area = abs((b_x_max - b_x_min) * (b_y_max - b_y_min))
+        union_area = a_area + b_area - intersection_area
+        return intersection_area / (float(union_area) + 1e-5)
+
+    def __get_best_iou_with_index(self, box, clustered_ws, clustered_hs):
+        cx, cy, w, h = box
+        x1 = cx - w * 0.5
+        y1 = cy - h * 0.5
+        x2 = cx + w * 0.5
+        y2 = cy + h * 0.5
+        true_box = [x1, y1, x2, y2]
+
+        best_iou = 0.0
+        best_iou_index = -1
+        for i in range(self.num_output_layers):
+            x1 = cx - clustered_ws[i] * 0.5
+            y1 = cy - clustered_hs[i] * 0.5
+            x2 = cx + clustered_ws[i] * 0.5
+            y2 = cy + clustered_hs[i] * 0.5
+            clustered_box = [x1, y1, x2, y2]
+            iou = self.__iou(true_box, clustered_box)
+            if iou > best_iou:
+                best_iou = iou
+                best_iou_index = i
+        return best_iou, best_iou_index
+
+    def __iou_with_clustered_wh(self, boxes, clustered_ws, clustered_hs):
+        iou_sum = 0.0
+        for true_box in boxes:
+            best_iou, _ = self.__get_best_iou_with_index(true_box, clustered_ws, clustered_hs)
+            iou_sum += best_iou
+        return iou_sum / float(len(boxes))
+
+    def cluster_wh(self):
+        def __load_label(__label_path):
+            with open(__label_path, 'rt') as __f:
+                __lines = __f.readlines()
+            return __lines
+
+        fs = []
+        for path in self.image_paths:
+            fs.append(self.pool.submit(__load_label, f'{path[:-4]}.txt'))
+
+        boxes, ws, hs = [], [], []
+        for f in tqdm(fs):
+            lines = f.result()
+            for line in lines:
+                class_index, cx, cy, w, h = list(map(float, line.split()))
+                boxes.append([cx, cy, w, h])
+                ws.append(w)
+                hs.append(h)
+
+        ws = np.asarray(ws).reshape((1, len(ws))).astype('float32')
+        hs = np.asarray(hs).reshape((1, len(hs))).astype('float32')
+
+        num_cluster = self.num_output_layers
+        criteria = (cv2.TERM_CRITERIA_EPS, -1, 1e-4)
+        width_compactness, _, ws = cv2.kmeans(ws, num_cluster, None, criteria, 100, cv2.KMEANS_RANDOM_CENTERS)
+        width_compactness /= float(len(self.image_paths))
+        print(f'width compactness  : {width_compactness:.7f}')
+        height_compactness, _, hs = cv2.kmeans(hs, num_cluster, None, criteria, 100, cv2.KMEANS_RANDOM_CENTERS)
+        height_compactness /= float(len(self.image_paths))
+        print(f'height compactness : {height_compactness:.7f}')
+
+        self.clustered_ws = sorted(np.asarray(ws).reshape(-1))
+        self.clustered_hs = sorted(np.asarray(hs).reshape(-1))
+        print(f'clustered  widths : ', end='')
+        for i in range(num_cluster):
+            print(f'{self.clustered_ws[i]:.4f} ', end='')
+        print()
+        print(f'clustered heights : ', end='')
+        for i in range(num_cluster):
+            print(f'{self.clustered_hs[i]:.4f} ', end='')
+        print()
+        print(f'avg IoU : {self.__iou_with_clustered_wh(boxes, self.clustered_ws, self.clustered_hs):.4f}')
 
     @staticmethod
     def __convert_to_boxes(label_lines):
@@ -147,27 +240,50 @@ class GeneratorFlow(tf.keras.utils.Sequence):
                 big_last_boxes = sorted(boxes, key=lambda __x: __x['area'], reverse=False)
                 small_last_boxes = sorted(boxes, key=lambda __x: __x['area'], reverse=True)
                 middle_last_boxes = self.__sort_middle_last(big_last_boxes, small_last_boxes)
+
                 layer_mapping_boxes = [small_last_boxes, middle_last_boxes, big_last_boxes]
+                # layer_mapping_boxes = [small_last_boxes, [], []]
+                # layer_mapping_boxes = [[], middle_last_boxes, []]
 
                 y = []
-                for i in range(len(self.output_shapes)):
+                for i in range(self.num_output_layers):
                     y.append(np.zeros((self.output_shapes[i][1], self.output_shapes[i][2], self.output_shapes[i][3]), dtype=np.float32))
 
-                for output_layer_index in range(len(self.output_shapes)):
+                for b in boxes:
+                    class_index, cx, cy, w, h = b['class_index'], b['cx'], b['cy'], b['w'], b['h']
+                    true_box = [cx, cy, w, h]
+                    # _, output_layer_index = self.__get_best_iou_with_index(true_box, self.clustered_ws, self.clustered_hs)
+                    output_layer_index = 1
+
                     output_rows = float(self.output_shapes[output_layer_index][1])
                     output_cols = float(self.output_shapes[output_layer_index][2])
-                    for b in layer_mapping_boxes[output_layer_index]:
-                        class_index, cx, cy, w, h = b['class_index'], b['cx'], b['cy'], b['w'], b['h']
-                        grid_width_ratio = 1 / output_cols
-                        grid_height_ratio = 1 / output_rows
-                        center_row = int(cy * output_rows)
-                        center_col = int(cx * output_cols)
-                        y[output_layer_index][center_row][center_col][0] = 1.0
-                        y[output_layer_index][center_row][center_col][1] = (cx - (center_col * grid_width_ratio)) / grid_width_ratio
-                        y[output_layer_index][center_row][center_col][2] = (cy - (center_row * grid_height_ratio)) / grid_height_ratio
-                        y[output_layer_index][center_row][center_col][3] = w
-                        y[output_layer_index][center_row][center_col][4] = h
-                        y[output_layer_index][center_row][center_col][int(class_index + 5)] = 1.0
+                    grid_width_ratio = 1 / output_cols
+                    grid_height_ratio = 1 / output_rows
+                    center_row = int(cy * output_rows)
+                    center_col = int(cx * output_cols)
+                    y[output_layer_index][center_row][center_col][0] = 1.0
+                    y[output_layer_index][center_row][center_col][1] = (cx - (center_col * grid_width_ratio)) / grid_width_ratio
+                    y[output_layer_index][center_row][center_col][2] = (cy - (center_row * grid_height_ratio)) / grid_height_ratio
+                    y[output_layer_index][center_row][center_col][3] = w
+                    y[output_layer_index][center_row][center_col][4] = h
+                    y[output_layer_index][center_row][center_col][int(class_index + 5)] = 1.0
+
+                # for output_layer_index in range(self.num_output_layers):
+                #     output_rows = float(self.output_shapes[output_layer_index][1])
+                #     output_cols = float(self.output_shapes[output_layer_index][2])
+                #     for b in layer_mapping_boxes[output_layer_index]:
+                #         class_index, cx, cy, w, h = b['class_index'], b['cx'], b['cy'], b['w'], b['h']
+                #         grid_width_ratio = 1 / output_cols
+                #         grid_height_ratio = 1 / output_rows
+                #         center_row = int(cy * output_rows)
+                #         center_col = int(cx * output_cols)
+                #         y[output_layer_index][center_row][center_col][0] = 1.0
+                #         y[output_layer_index][center_row][center_col][1] = (cx - (center_col * grid_width_ratio)) / grid_width_ratio
+                #         y[output_layer_index][center_row][center_col][2] = (cy - (center_row * grid_height_ratio)) / grid_height_ratio
+                #         y[output_layer_index][center_row][center_col][3] = w
+                #         y[output_layer_index][center_row][center_col][4] = h
+                #         y[output_layer_index][center_row][center_col][int(class_index + 5)] = 1.0
+
                 batch_y1.append(y[0])
                 batch_y2.append(y[1])
                 batch_y3.append(y[2])
@@ -176,10 +292,6 @@ class GeneratorFlow(tf.keras.utils.Sequence):
             batch_y2 = np.asarray(batch_y2).astype('float32')
             batch_y3 = np.asarray(batch_y3).astype('float32')
             sleep(0)
-            # print(np.shape(batch_y1))
-            # print(np.shape(batch_y2))
-            # print(np.shape(batch_y3))
-            # exit(1)
             self.batch_q.put((batch_x, [batch_y1, batch_y2, batch_y3]), block=True)
 
     def __get_next_batch_image_paths(self):
