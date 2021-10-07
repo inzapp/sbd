6179ec9ca99b51c46e7bfb7767e63a958dbfd052
inzapp <inzapp@naver.com>
2021-10-07 17:00:11 +0900

    [yolo.py] step lr decay 0.8 -> 0.7 iter에 적용

--- yolo.py
+++ yolo.py
@@ -185,9 +185,7 @@ class Yolo:
                 self.__save_model(iteration_count=iteration_count)
                 if self.__training_view:
                     self.__training_view_function()
-                if iteration_count == int(self.__iterations * 0.5):
-                    tf.keras.backend.set_value(self.__model.optimizer.lr, self.__model.optimizer.lr * 0.1)
-                elif iteration_count == int(self.__iterations * 0.75):
+                if iteration_count == int(self.__iterations * 0.7):
                     tf.keras.backend.set_value(self.__model.optimizer.lr, self.__model.optimizer.lr * 0.1)
                 elif iteration_count == self.__iterations:
                     print('\n\ntrain end successfully')
