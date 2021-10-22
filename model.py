dec4b604a79ea60167a0c47b7defc91ddf4efcd2
inzapp <inzapp@naver.com>
2021-10-22 09:34:56 +0900

    [model.py] add __lp_detection_sbd_csp

--- model.py
+++ model.py
@@ -51,11 +51,12 @@ class Model:
         # return self.__vgg_16()
         # return self.__darknet_53()
         # return self.__lp_detection_sbd()
+        return self.__lp_detection_sbd_csp()
         # return self.__person_detail()
         # return self.__200m_big()
         # return self.__200m_big_csp()
         # return self.__64_64_crop()
-        return self.__64_64_crop_csp()
+        # return self.__64_64_crop_csp()
         # return self.__tiny_yolo_v3_no_upscale()
         # return self.__tiny_yolo_v4_no_upscale()
         # return self.__loon()
@@ -125,21 +126,21 @@ class Model:
         x = self.__drop_filter(x, 0.0625)
         x = self.__csp_block(128, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(128, 3, x, bn=True)
+        x = self.__conv_block(128, 3, x, bn=True)
         y1 = self.__detection_layer(x, 'output_1')
         x = self.__avg_max_pool(x)
 
         x = self.__drop_filter(x, 0.0625)
         x = self.__csp_block(256, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(256, 3, x, bn=True)
+        x = self.__conv_block(256, 3, x, bn=True)
         y2 = self.__detection_layer(x, 'output_2')
         x = self.__avg_max_pool(x)
 
         x = self.__drop_filter(x, 0.0625)
         x = self.__csp_block(512, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(512, 3, x, bn=True)
+        x = self.__conv_block(512, 3, x, bn=True)
         y3 = self.__detection_layer(x, 'output_3')
         return tf.keras.models.Model(input_layer, [y1, y2, y3])
 
@@ -407,6 +408,34 @@ class Model:
         x = self.__max_pool(x)
 
         x = self.__conv_block(128, 3, x, bn=True, activation_first=True)
+        y3 = self.__detection_layer(x, 'output_3')
+        return tf.keras.models.Model(input_layer, [y1, y2, y3])
+
+    def __lp_detection_sbd_csp(self):
+        input_layer = tf.keras.layers.Input(shape=self.__input_shape)
+        x = self.__conv_block(8, 3, input_layer, bn=True)
+        x = self.__avg_max_pool(x)
+
+        x = self.__drop_filter(x, 0.0625)
+        x = self.__conv_block(16, 3, x, bn=False)
+        x = self.__avg_max_pool(x)
+
+        x = self.__drop_filter(x, 0.0625)
+        x = self.__conv_block(32, 3, x, bn=False)
+        x = self.__avg_max_pool(x)
+
+        x = self.__drop_filter(x, 0.0625)
+        x = self.__csp_block(64, 3, x, bn=True)
+        y1 = self.__detection_layer(x, 'output_1')
+        x = self.__avg_max_pool(x)
+
+        x = self.__drop_filter(x, 0.0625)
+        x = self.__csp_block(128, 3, x, bn=True)
+        y2 = self.__detection_layer(x, 'output_2')
+        x = self.__avg_max_pool(x)
+
+        x = self.__drop_filter(x, 0.0625)
+        x = self.__csp_block(128, 3, x, bn=True)
         y3 = self.__detection_layer(x, 'output_3')
         return tf.keras.models.Model(input_layer, [y1, y2, y3])
 
