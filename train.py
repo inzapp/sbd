88476b0268da9af382f530e72da8391d9a5da3bf
inzapp <inzapp@naver.com>
2021-10-22 09:36:18 +0900

    c

--- train.py
+++ train.py
@@ -72,6 +72,11 @@ if __name__ == '__main__':
     """
 
     Yolo(
+        # train_image_path=r'C:\inz\train_data\loon\train',
+        # validation_image_path=r'C:\inz\train_data\loon\validation',
+        # class_names_file_path=r'C:\inz\train_data\loon\train\classes.txt',
+        # input_shape=(128, 512, 3),
+
         # train_image_path=r'C:\inz\train_data\200m_sequence_for_auto_label\train',
         # validation_image_path=r'C:\inz\train_data\200m_sequence_for_auto_label\validation',
         # class_names_file_path=r'C:\inz\train_data\200m_sequence_for_auto_label\train\classes.txt',
@@ -83,41 +88,35 @@ if __name__ == '__main__':
         # class_names_file_path=r'T:\200m_big_small_detection\train_data\small\small_all\train\classes.txt',
         # input_shape=(192, 576, 1),
 
-        # train_image_path=r'C:\inz\train_data\loon\train',
-        # validation_image_path=r'C:\inz\train_data\loon\validation',
-        # class_names_file_path=r'C:\inz\train_data\loon\train\classes.txt',
-        # input_shape=(128, 512, 3),
-
         # pretrained_model_path=r'C:\inz\git\yolo-lab\checkpoints\crop_64_64\model_465000_iter_mAP_0.9574_f1_0.9586_tp_iou_0.7621.h5',
-        train_image_path=r'C:\inz\train_data\crop_copy_for_sbd\train',
-        validation_image_path=r'C:\inz\train_data\crop_copy_for_sbd\validation',
-        class_names_file_path=r'C:\inz\train_data\crop_copy_for_sbd\train\classes.txt',
-        input_shape=(64, 64, 1),
+        # train_image_path=r'C:\inz\train_data\crop_copy_for_sbd\train',
+        # validation_image_path=r'C:\inz\train_data\crop_copy_for_sbd\validation',
+        # class_names_file_path=r'C:\inz\train_data\crop_copy_for_sbd\train\classes.txt',
+        # input_shape=(64, 64, 1),
 
         # train_image_path=r'list/crop_with_unknown_class/train.txt',
         # validation_image_path=r'list/crop_with_unknown_class/validation.txt',
         # class_names_file_path=r'T:/200m_big_small_detection/train_data/crop/classes.txt',
         # input_shape=(64, 64, 1),
 
-        # pretrained_model_path=r'model_last.h5',
-        # train_image_path=r'T:\200m_big_small_detection\train_data\under_big\big_yj\train',
-        # validation_image_path=r'T:\200m_big_small_detection\train_data\under_big\big_yj\validation',
-        # class_names_file_path=r'T:\200m_big_small_detection\train_data\under_big\big_yj\train\classes.txt',
-        # input_shape=(416, 416, 1),
+        train_image_path=r'C:\inz\train_data\lp_detection\train',
+        validation_image_path=r'C:\inz\train_data\lp_detection\validation',
+        class_names_file_path=r'C:\inz\train_data\lp_detection\train\classes.txt',
+        input_shape=(384, 640, 1),
 
         lr=0.001,
         decay=0.0005,
         momentum=0.9,
         burn_in=1000,
         batch_size=4,
-        iterations=1000000,
+        iterations=2558000,
         curriculum_iterations=0,
         training_view=False,
         map_checkpoint=True).fit()
 
     # from glob import glob
     # from random import shuffle
-    # model_path = r'C:\inz\git\yolo-lab-3-layer-refactoring\checkpoints\200m\yolo_style\model_84000_iter_mAP_0.1575_f1_0.2982.h5'
+    # model_path = r'C:\inz\git\yolo-lab\checkpoints\model_66000_iter_mAP_0.8389_f1_0.8627_tp_iou_0.8111.h5'
     # model = Yolo(pretrained_model_path=model_path, class_names_file_path=r'X:\200m_detection\origin\train\classes.txt', test_only=True)
     # paths = glob(r'Z:\07. SW 개발팀\08. 개인 폴더\24. 표성백\22.돌발상황감지\2021-05-17\*.mp4')
     # shuffle(paths)
