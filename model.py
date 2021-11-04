0576dac8f0a42db78da46743a605562cec798a40
inzapp <inzapp@naver.com>
2021-11-04 10:11:06 +0900

    [model.py] remove csp in person detail model

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ class Model:
         # return self.__lp_detection_sbd()
         # return self.__lp_detection_sbd_csp()
         # return self.__person_detail()
-        return self.__person_detail_csp()
+        return self.__person_detail_new()
         # return self.__lcd_csp()
         # return self.__200m_big()
         # return self.__200m_big_csp()
@@ -364,7 +364,7 @@ class Model:
         y3 = self.__detection_layer(x, 'output_3')
         return tf.keras.models.Model(input_layer, [y1, y2, y3])
 
-    def __person_detail_csp(self):
+    def __person_detail_new(self):
         input_layer = tf.keras.layers.Input(shape=self.__input_shape)
         x = self.__conv_block(16, 3, input_layer, bn=True)
         x = self.__avg_max_pool(x)
@@ -378,21 +378,21 @@ class Model:
         x = self.__avg_max_pool(x)
 
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(128, 3, x, bn=False)
+        x = self.__conv_block(128, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
         x = self.__conv_block(128, 3, x, bn=True)
         y1 = self.__detection_layer(x, 'output_1')
         x = self.__avg_max_pool(x)
 
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(256, 3, x, bn=False)
+        x = self.__conv_block(256, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
         x = self.__conv_block(256, 3, x, bn=True)
         y2 = self.__detection_layer(x, 'output_2')
         x = self.__avg_max_pool(x)
 
         x = self.__drop_filter(x, 0.0625)
-        x = self.__csp_block(256, 3, x, bn=False)
+        x = self.__conv_block(256, 3, x, bn=False)
         x = self.__drop_filter(x, 0.0625)
         x = self.__conv_block(256, 3, x, bn=True)
         y3 = self.__detection_layer(x, 'output_3')
