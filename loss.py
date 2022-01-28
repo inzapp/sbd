3d164f0b71663feb32bf03e918865d70e1a62d23
inzapp <inzapp@naver.com>
2022-01-28 17:56:49 +0900

    bbox iou loss, bbox xywh loss 주석으로 선택할 수 있게 수정

--- loss.py
+++ loss.py
@@ -41,64 +41,59 @@ def __confidence_loss(y_true, y_pred):
     return loss 
 
 
-# def __iou(y_true, y_pred):
-#     y_true_shape = tf.cast(tf.shape(y_true), dtype=tf.dtypes.float32)
-#     grid_height, grid_width = y_true_shape[1], y_true_shape[2]
-#     # eps = tf.keras.backend.epsilon()
-# 
-#     cx_true = y_true[:, :, :, 1]
-#     cy_true = y_true[:, :, :, 2]
-#     w_true = y_true[:, :, :, 3]
-#     h_true = y_true[:, :, :, 4]
-#     # w_true = tf.sqrt(y_true[:, :, :, 3] + eps)
-#     # h_true = tf.sqrt(y_true[:, :, :, 4] + eps)
-# 
-#     x_range = tf.range(grid_width, dtype=tf.dtypes.float32)
-#     x_offset = tf.broadcast_to(x_range, shape=tf.shape(cx_true))
-# 
-#     y_range = tf.range(grid_height, dtype=tf.dtypes.float32)
-#     y_range = tf.reshape(y_range, shape=(1, grid_height, 1))
-#     y_offset = tf.broadcast_to(y_range, shape=tf.shape(cy_true))
-# 
-#     cx_true = x_offset + (cx_true * 1.0 / grid_width)
-#     cy_true = y_offset + (cy_true * 1.0 / grid_height)
-# 
-#     x1_true = cx_true - w_true / 2.0
-#     y1_true = cy_true - h_true / 2.0
-#     x2_true = cx_true + w_true / 2.0
-#     y2_true = cy_true + h_true / 2.0
-# 
-#     cx_pred = y_pred[:, :, :, 1]
-#     cy_pred = y_pred[:, :, :, 2]
-#     w_pred = y_pred[:, :, :, 3]
-#     h_pred = y_pred[:, :, :, 4]
-#     # w_pred = tf.sqrt(y_pred[:, :, :, 3] + eps)
-#     # h_pred = tf.sqrt(y_pred[:, :, :, 4] + eps)
-# 
-#     cx_pred = x_offset + (cx_pred * 1.0 / grid_width)
-#     cy_pred = y_offset + (cy_pred * 1.0 / grid_height)
-# 
-#     x1_pred = cx_pred - w_pred / 2.0
-#     y1_pred = cy_pred - h_pred / 2.0
-#     x2_pred = cx_pred + w_pred / 2.0
-#     y2_pred = cy_pred + h_pred / 2.0
-# 
-#     min_x2 = tf.minimum(x2_true, x2_pred)
-#     max_x1 = tf.maximum(x1_true, x1_pred)
-#     min_y2 = tf.minimum(y2_true, y2_pred)
-#     max_y1 = tf.maximum(y1_true, y1_pred)
-# 
-#     intersection_width = tf.maximum(min_x2 - max_x1, 0.0)
-#     intersection_height = tf.maximum(min_y2 - max_y1, 0.0)
-#     intersection = intersection_width * intersection_height
-# 
-#     y_true_area = w_true * h_true
-#     y_pred_area = w_pred * h_pred
-#     union = y_true_area + y_pred_area - intersection
-#     return intersection / (union + 1e-5)
+def __iou(y_true, y_pred):
+    y_true_shape = tf.cast(tf.shape(y_true), dtype=tf.dtypes.float32)
+    grid_height, grid_width = y_true_shape[1], y_true_shape[2]
+
+    cx_true = y_true[:, :, :, 1]
+    cy_true = y_true[:, :, :, 2]
+    w_true = y_true[:, :, :, 3]
+    h_true = y_true[:, :, :, 4]
+
+    x_range = tf.range(grid_width, dtype=tf.dtypes.float32)
+    x_offset = tf.broadcast_to(x_range, shape=tf.shape(cx_true))
+
+    y_range = tf.range(grid_height, dtype=tf.dtypes.float32)
+    y_range = tf.reshape(y_range, shape=(1, grid_height, 1))
+    y_offset = tf.broadcast_to(y_range, shape=tf.shape(cy_true))
+
+    cx_true = x_offset + (cx_true * 1.0 / grid_width)
+    cy_true = y_offset + (cy_true * 1.0 / grid_height)
+
+    x1_true = cx_true - w_true / 2.0
+    y1_true = cy_true - h_true / 2.0
+    x2_true = cx_true + w_true / 2.0
+    y2_true = cy_true + h_true / 2.0
+
+    cx_pred = y_pred[:, :, :, 1]
+    cy_pred = y_pred[:, :, :, 2]
+    w_pred = y_pred[:, :, :, 3]
+    h_pred = y_pred[:, :, :, 4]
+
+    cx_pred = x_offset + (cx_pred * 1.0 / grid_width)
+    cy_pred = y_offset + (cy_pred * 1.0 / grid_height)
+
+    x1_pred = cx_pred - w_pred / 2.0
+    y1_pred = cy_pred - h_pred / 2.0
+    x2_pred = cx_pred + w_pred / 2.0
+    y2_pred = cy_pred + h_pred / 2.0
+
+    min_x2 = tf.minimum(x2_true, x2_pred)
+    max_x1 = tf.maximum(x1_true, x1_pred)
+    min_y2 = tf.minimum(y2_true, y2_pred)
+    max_y1 = tf.maximum(y1_true, y1_pred)
+
+    intersection_width = tf.maximum(min_x2 - max_x1, 0.0)
+    intersection_height = tf.maximum(min_y2 - max_y1, 0.0)
+    intersection = intersection_width * intersection_height
+
+    y_true_area = w_true * h_true
+    y_pred_area = w_pred * h_pred
+    union = y_true_area + y_pred_area - intersection
+    return intersection / (union + 1e-5)
 
 
-def __bbox_loss(y_true, y_pred):
+def __bbox_loss_xywh(y_true, y_pred):
     obj_true = y_true[:, :, :, 0]
     obj_count = tf.cast(tf.reduce_sum(obj_true), tf.float32)
     if tf.equal(obj_count, tf.constant(0.0)):
@@ -117,6 +112,23 @@ def __bbox_loss(y_true, y_pred):
     wh_loss = tf.reduce_mean(wh_loss, axis=-1) * obj_true
     wh_loss = tf.reduce_sum(wh_loss)
     return (xy_loss + wh_loss) * 5.0
+
+
+def __bbox_loss_iou(y_true, y_pred):
+    obj_true = y_true[:, :, :, 0]
+    obj_count = tf.cast(tf.reduce_sum(obj_true), tf.float32)
+    if tf.equal(obj_count, tf.constant(0.0)):
+        return 0.0
+
+    loss = tf.losses.binary_crossentropy(obj_true, __iou(y_true, y_pred) * obj_true)
+    loss = tf.reduce_mean(loss, axis=0)
+    loss = tf.reduce_sum(loss)
+    return loss
+
+
+def __bbox_loss(y_true, y_pred):
+    return __bbox_loss_iou(y_true, y_pred)
+    # return __bbox_loss_xywh(y_true, y_pred)
 
 
 def __classification_loss(y_true, y_pred):
