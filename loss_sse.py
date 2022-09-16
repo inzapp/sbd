bfc1e916863d47f3e7b5df002e99e74c7d8f4dbc
inzapp <inzapp@naver.com>
2022-09-16 11:22:28 +0900

    [loss_export.py] rename loss_export.py -> loss_sse.py, refine code loss_sse.py

--- loss_sse.py
+++ loss_sse.py
@@ -22,50 +22,40 @@ from keras import backend as K
 from tensorflow.python.framework.ops import convert_to_tensor_v2
 
 
-def __confidence_loss(y_true, y_pred, ignore_threshold=ignore_threshold):
+def __confidence_loss(y_true, y_pred, ignore_threshold=1.0):
     obj_true = y_true[:, :, :, 0]
     obj_pred = y_pred[:, :, :, 0]
-    loss = K.square(obj_true - obj_pred) * tf.where(obj_true * obj_pred > ignore_threshold, 0.0, 1.0)
-    loss = K.mean(loss, axis=0)
-    loss = K.sum(loss)
+    loss = tf.square(obj_true - obj_pred) * tf.where(obj_true * obj_pred > ignore_threshold, 0.0, 1.0)
+    loss = tf.reduce_sum(tf.reduce_mean(loss, axis=0))
     return loss
 
 
 def __bbox_loss(y_true, y_pred):
     obj_true = y_true[:, :, :, 0]
-    obj_count = K.cast_to_floatx(K.sum(obj_true))
-    if K.equal(obj_count, K.constant(0.0)):
+    obj_count = K.cast_to_floatx(tf.reduce_sum(obj_true))
+    if tf.equal(obj_count, tf.constant(0.0)):
         return 0.0
 
     xy_true = y_true[:, :, :, 1:3]
     xy_pred = y_pred[:, :, :, 1:3]
-    xy_loss = K.square(xy_true - xy_pred)
-    xy_loss = K.sum(xy_loss, axis=-1) * obj_true
-    xy_loss = K.mean(xy_loss, axis=0)
-    xy_loss = K.sum(xy_loss)
+    xy_loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(tf.square(xy_true - xy_pred), axis=-1) * obj_true, axis=0))
 
     eps = K.epsilon()
-    wh_true = K.sqrt(y_true[:, :, :, 3:5] + eps)
-    wh_pred = K.sqrt(y_pred[:, :, :, 3:5] + eps)
-    wh_loss = K.square(wh_true - wh_pred) * obj_true
-    wh_loss = K.sum(wh_loss, axis=-1)
-    wh_loss = K.mean(wh_loss, axis=0)
-    wh_loss = K.sum(wh_loss)
+    wh_true = tf.sqrt(y_true[:, :, :, 3:5] + eps)
+    wh_pred = tf.sqrt(y_pred[:, :, :, 3:5] + eps)
+    wh_loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(tf.square(wh_true - wh_pred), axis=-1) * obj_true, axis=0))
     return (xy_loss + wh_loss) * 5.0
 
 
 def __classification_loss(y_true, y_pred):
     obj_true = y_true[:, :, :, 0]
-    obj_count = K.cast_to_floatx(K.sum(obj_true))
-    if K.equal(obj_count, K.constant(0.0)):
+    obj_count = K.cast_to_floatx(tf.reduce_sum(obj_true))
+    if tf.equal(obj_count, tf.constant(0.0)):
         return 0.0
 
     class_true = y_true[:, :, :, 5:]
     class_pred = y_pred[:, :, :, 5:]
-    loss = K.square(class_true - class_pred)
-    loss = K.sum(loss, axis=-1) * obj_true
-    loss = K.mean(loss, axis=0)
-    loss = K.sum(loss)
+    loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(tf.square(class_true - class_pred), axis=-1) * obj_true, axis=0))
     return loss
 
 
@@ -81,7 +71,7 @@ def confidence_with_bbox_loss(y_true, y_pred):
     return __confidence_loss(y_true, y_pred) + __bbox_loss(y_true, y_pred)
 
 
-def yolo_loss(y_true, y_pred, ignore_threshold=0.8):
+def yolo_loss(y_true, y_pred, ignore_threshold=1.0):
     y_pred = convert_to_tensor_v2(y_pred)
     y_true = K.cast(y_true, y_pred.dtype)
-    return __confidence_loss(y_true, y_pred, ignore_threshold=ignore_threshold) + __bbox_loss(y_true, y_pred) + __classification_loss(y_true, y_pred)
+    return __confidence_loss(y_true, y_pred, ignore_threshold=1.0) + __bbox_loss(y_true, y_pred) + __classification_loss(y_true, y_pred)
