8203c584cee7267175fc281d7ce04c82cd4f1ed5
inzapp <inzapp@naver.com>
2022-10-12 09:15:55 +0900

    change naming model checkpoint naming : tp_iou -> iou

--- yolo.py
+++ yolo.py
@@ -265,8 +265,8 @@ class Yolo:
         ul = str(Yolo.g_use_layers) if len(Yolo.g_use_layers) > 0 else 'all'
         if use_map_checkpoint:
             self.__model.save('model.h5', include_optimizer=False)
-            mean_ap, f1_score, tp_iou, tp, fp, fn, confidence = calc_mean_average_precision(self.__model, self.__validation_image_paths)
-            self.__model.save(f'{self.__checkpoints}/model_{iteration_count}_iter_mAP_{mean_ap:.4f}_f1_{f1_score:.4f}_tp_iou_{tp_iou:.4f}_tp_{tp}_fp_{fp}_fn_{fn}_conf_{confidence:.4f}_ul_{ul}.h5', include_optimizer=False)
+            mean_ap, f1_score, iou, tp, fp, fn, confidence = calc_mean_average_precision(self.__model, self.__validation_image_paths)
+            self.__model.save(f'{self.__checkpoints}/model_{iteration_count}_iter_mAP_{mean_ap:.4f}_f1_{f1_score:.4f}_iou_{iou:.4f}_tp_{tp}_fp_{fp}_fn_{fn}_conf_{confidence:.4f}_ul_{ul}.h5', include_optimizer=False)
             self.__model.save(f'model_last_ul_{ul}.h5', include_optimizer=False)
         else:
             self.__model.save(f'{self.__checkpoints}/model_{iteration_count}_iter_ul_{ul}.h5', include_optimizer=False)
