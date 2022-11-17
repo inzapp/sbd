79b5f10da5abf245a70e1ec5b08eeeabe3d94764
inzapp <inzapp@naver.com>
2022-11-17 09:07:29 +0900

    update loss_sse_sse.py to all reduce_sum / batch_size

--- loss_sse_sse.py
+++ loss_sse_sse.py
@@ -25,9 +25,10 @@ def __confidence_loss(y_true, y_pred):
     obj_true = y_true[:, :, :, 0]
     obj_pred = y_pred[:, :, :, 0]
     loss = tf.square(obj_true - obj_pred)
-    obj_loss = tf.reduce_sum(tf.reduce_mean(loss * obj_true, axis=0))
-    background_loss = tf.reduce_sum(tf.reduce_mean(loss * (1.0 - obj_true), axis=0))
-    return obj_loss + (background_loss * 0.5)
+    obj_loss = tf.reduce_sum(loss * obj_true)
+    background_loss = tf.reduce_sum(loss * (1.0 - obj_true))
+    loss = obj_loss + (background_loss * 0.5)
+    return loss / tf.cast(tf.shape(y_true)[0], dtype=y_pred.dtype)
 
 
 def __bbox_loss(y_true, y_pred):
@@ -38,13 +39,14 @@ def __bbox_loss(y_true, y_pred):
 
     xy_true = y_true[:, :, :, 1:3]
     xy_pred = y_pred[:, :, :, 1:3]
-    xy_loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(tf.square(xy_true - xy_pred), axis=-1) * obj_true, axis=0))
+    xy_loss = tf.reduce_sum(tf.reduce_sum(tf.square(xy_true - xy_pred), axis=-1) * obj_true)
 
     eps = tf.keras.backend.epsilon()
     wh_true = tf.sqrt(y_true[:, :, :, 3:5] + eps)
     wh_pred = tf.sqrt(y_pred[:, :, :, 3:5] + eps)
-    wh_loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(tf.square(wh_true - wh_pred), axis=-1) * obj_true, axis=0))
-    return (xy_loss + wh_loss) * 5.0
+    wh_loss = tf.reduce_sum(tf.reduce_sum(tf.square(wh_true - wh_pred), axis=-1) * obj_true)
+    loss = (xy_loss + wh_loss) * 5.0
+    return loss / tf.cast(tf.shape(y_true)[0], dtype=y_pred.dtype)
 
 
 def __classification_loss(y_true, y_pred):
@@ -58,23 +60,23 @@ def __classification_loss(y_true, y_pred):
     loss = tf.square(class_true - class_pred)
     class_true_loss = loss * class_true
     class_false_loss = loss * (1.0 - class_true)
-    loss = tf.reduce_sum(tf.reduce_mean(tf.reduce_sum(class_true_loss + class_false_loss, axis=-1) * obj_true, axis=0))
-    return loss
+    loss = tf.reduce_sum(tf.reduce_sum(class_true_loss + class_false_loss, axis=-1) * obj_true)
+    return loss / tf.cast(tf.shape(y_true)[0], dtype=y_pred.dtype)
 
 
-def confidence_loss(y_true, y_pred, focal_gamma):
+def confidence_loss(y_true, y_pred, alpha, gamma, label_smoothing):
     y_pred = convert_to_tensor_v2(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     return __confidence_loss(y_true, y_pred)
 
 
-def confidence_with_bbox_loss(y_true, y_pred, focal_gamma):
+def confidence_with_bbox_loss(y_true, y_pred, alpha, gamma, label_smoothing):
     y_pred = convert_to_tensor_v2(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     return __confidence_loss(y_true, y_pred) + __bbox_loss(y_true, y_pred)
 
 
-def yolo_loss(y_true, y_pred, focal_gamma):
+def yolo_loss(y_true, y_pred, alpha, gamma, label_smoothing):
     y_pred = convert_to_tensor_v2(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
     return __confidence_loss(y_true, y_pred) + __bbox_loss(y_true, y_pred) + __classification_loss(y_true, y_pred)
