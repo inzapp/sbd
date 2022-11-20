2f1e923f67727097d56469009a625ad88456dfd2
inzapp <inzapp@naver.com>
2022-11-20 17:21:26 +0900

    add map.py

---
+++ map.py
@@ -0,0 +1,26 @@
+"""
+Authors : inzapp
+
+Github url : https://github.com/inzapp/c-yolo
+
+Copyright 2021 inzapp Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"),
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from yolo import Yolo
+from train_config import config
+
+if __name__ == '__main__':
+    config['pretrained_model_path'] = r'model_last.h5'
+    Yolo(config=config).calculate_map('validation', save_model=False)
+
