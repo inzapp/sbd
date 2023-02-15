d4a7cddf7bf88bf26254574eff7c602d1f9826c6
inzapp <inzapp@naver.com>
2023-02-15 13:26:01 +0900

    update auto_label.py : code refactor to use ModelUtil, clip bbox coordinate before save

--- auto_label.py
+++ auto_label.py
@@ -1,25 +1,24 @@
 import os
-import shutil as sh
-from concurrent.futures import ThreadPoolExecutor
-from glob import glob
-
+import cv2
 import numpy as np
+import shutil as sh
 import tensorflow as tf
-from cv2 import cv2
-from tqdm import tqdm
 
+from glob import glob
+from tqdm import tqdm
 from yolo import Yolo
 from util import ModelUtil
+from concurrent.futures.thread import ThreadPoolExecutor
 
 
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 g_confidence_threshold = 0.5
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
 def auto_label(model_path, image_path, origin_classes_txt_path):
     model = tf.keras.models.load_model(model_path, compile=False)
     input_shape = model.input_shape[1:]
-    w, h, channel = ModelUtil.get_width_height_channel_from_input_shape(input_shape)
+    _, _, channel = ModelUtil.get_width_height_channel_from_input_shape(input_shape)
     color_mode = cv2.IMREAD_GRAYSCALE if channel == 1 else cv2.IMREAD_COLOR
 
     image_paths = glob(f'{image_path}/*.jpg')
@@ -34,33 +33,29 @@ def auto_label(model_path, image_path, origin_classes_txt_path):
     for path in image_paths:
         fs.append(pool.submit(ModelUtil.load_img, path, channel))
 
+    # device = ModelUtil.available_device()
     device = 'cpu'
-    for d in tf.config.list_physical_devices():
-        if str(d).find('GPU') > -1:
-            device = 'gpu'
-            break
-
     for f in tqdm(fs):
         raw, _, path = f.result()
-        res = Yolo.predict(model, raw, device, confidence_threshold=g_confidence_threshold)
+        boxes = Yolo.predict(model, raw, device, confidence_threshold=g_confidence_threshold)
         label_content = ''
-        for i in range(len(res)):
-            class_index = res[i]['class']
-            xmin, ymin, xmax, ymax = res[i]['bbox_norm']
+        for box in boxes:
+            class_index = box['class']
+            xmin, ymin, xmax, ymax = box['bbox_norm']
             w = xmax - xmin
             h = ymax - ymin
-            cx = xmin + w * 0.5
-            cy = ymin + h * 0.5
+            cx = xmin + (w * 0.5)
+            cy = ymin + (h * 0.5)
+            cx, cy, w, h = np.clip(np.array([cx, cy, w, h]), 0.0, 1.0)
             label_content += f'{class_index} {cx:.6f} {cy:.6f} {w:.6f} {h:.6f}\n'
-
         with open(f'{path[:-4]}.txt', 'wt') as file:
-            file.writelines(label_content)
+            file.write(label_content)
 
 
 def main():
-    model_path = r'C:\inz\git\yolo-lab\checkpoints\model_3000_iter_mAP_1.0000_f1_1.0000_iou_0.8663_tp_104_fp_0_fn_0_conf_0.8196_ul_all.h5'
-    origin_classes_txt_path = r'C:\inz\train_data\loon\validation_copy\classes.txt'
-    img_path = r'C:\inz\train_data\loon\validation_copy'
+    model_path = r'C:\inz\git\yolo-lab\checkpoint\loon\loon_10000_iter_mAP_1.0000_f1_1.0000_iou_0.8646_tp_104_fp_0_fn_0_conf_0.6846_confth_0.10_tpiouth_0.50.h5'
+    origin_classes_txt_path = r'C:\inz\train_data\loon\train\classes.txt'
+    img_path = r'C:\inz\train_data\loon\validation_autolabel_test'
     auto_label(model_path, img_path, origin_classes_txt_path)
 
 
