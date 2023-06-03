85dadb225041065c2cfc2cd1d79d8e0bffccf296
inzapp <inzapp@naver.co.kr>
2023-06-03 14:49:25 +0900

    update setup shell files to Dockerfile

--- setup/run_docker.sh
+++ setup/run_docker.sh
@@ -1,10 +1,17 @@
-TF_VERSION=2.5.1
 TRAIN_DATA_PATH=~/train_data
+
+IMAGE=sbd:latest
 CONTAINER_NAME=sbd
-TENSORFLOW=tensorflow/tensorflow:$TF_VERSION-gpu
 SBD_PATH=$(pwd)/../
 
-if [ "$(docker ps | grep $TENSORFLOW | awk '{print $2}')" != "$TENSORFLOW" ]; then
-    docker run -d --name $CONTAINER_NAME -it --rm --gpus all -v $TRAIN_DATA_PATH:/train_data -v $SBD_PATH:/root/sbd $TENSORFLOW /bin/bash
+if [ "$(docker images | grep $CONTAINER_NAME | awk '{print $1}')" != "$CONTAINER_NAME" ]; then
+    echo 'SBD docker image not found.'
+    echo 'Build docker image first with command "docker build --no-cache -t sbd -f Dockerfile.cuxxx ."'
+    exit
 fi
+
+if [ "$(docker ps | grep $IMAGE | awk '{print $2}')" != "$IMAGE" ]; then
+    docker run -u $(id -u):$(id -g) -d --name $CONTAINER_NAME -it --rm --gpus all -v $TRAIN_DATA_PATH:/train_data -v $SBD_PATH:/sbd $IMAGE /bin/bash
+fi
+
 docker exec -it $CONTAINER_NAME /bin/bash
