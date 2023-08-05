9bfaf32f8289adab070b4b3567769af75eda2d2d
inzapp <inzapp@naver.co.kr>
2023-08-05 18:52:21 +0900

    update docker run script to using docker pull

--- setup/run_docker.sh
+++ setup/run_docker.sh
@@ -1,17 +1,24 @@
-TRAIN_DATA_PATH=~/train_data
-
-IMAGE=sbd:latest
+### ----- user define vars -----
+CUDA_VERSION=cu118
 CONTAINER_NAME=sbd
-SBD_PATH=$(pwd)/../
+TRAIN_DATA_PATH=~/train_data
+### ----- user define vars -----
 
-if [ "$(docker images | grep $CONTAINER_NAME | awk '{print $1}')" != "$CONTAINER_NAME" ]; then
-    echo 'SBD docker image not found.'
-    echo 'Build docker image first with command "docker build --no-cache -t sbd -f Dockerfile.cuxxx ."'
+SBD_PATH=$(pwd)/../
+REPOSITORY_NAME=inzapp/sbd
+AVAILABLE_CUDA_VERSIONS=("cu102" "cu110" "cu112" "cu118")
+if [[ ! " ${AVAILABLE_CUDA_VERSIONS[*]} " == *" $CUDA_VERSION "* ]]; then
+    echo "$CUDA_VERSION is nvalid cuda version. available cuda versions : ${AVAILABLE_CUDA_VERSIONS[*]}"
     exit
 fi
 
-if [ "$(docker ps | grep $IMAGE | awk '{print $2}')" != "$IMAGE" ]; then
-    docker run -u $(id -u):$(id -g) -d --name $CONTAINER_NAME -it --rm --gpus all -v $TRAIN_DATA_PATH:/train_data -v $SBD_PATH:/sbd $IMAGE /bin/bash
+if [ "$(docker images | grep $REPOSITORY_NAME | grep $CUDA_VERSION | awk '{print $2}')" == "$CUDA_VERSION" ]; then
+    docker run -u $(id -u):$(id -g) -d --name $CONTAINER_NAME -it --rm --gpus all -v $TRAIN_DATA_PATH:/train_data -v $SBD_PATH:/sbd $REPOSITORY_NAME:$CUDA_VERSION /bin/bash
+else
+    echo "Docker image not found : $REPOSITORY_NAME:$CUDA_VERSION"
+    echo "Pull docker image using 'docker pull inzapp/sbd:$CUDA_VERSION'"
+    echo "or build docker image with command 'docker build --no-cache -t $REPOSITORY_NAME:$CUDA_VERSION -f Dockerfile.$CUDA_VERSION .'"
+    exit
 fi
 
 docker exec -it $CONTAINER_NAME /bin/bash
